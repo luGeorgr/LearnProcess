e12004946a0861aa26753204e164906598807013
rouqiu <rouqiu@qunhemail.com>
2018-08-16 11:15:28 +0800

    learn volatile

--- src/main/java/www/george/com/thread/synch/SynchronizedTest.java
+++ src/main/java/www/george/com/thread/synch/SynchronizedTest.java
@@ -5,7 +5,24 @@ package www.george.com.thread.synch;
  *
  */
 public class SynchronizedTest {
-    public static void main(String[] argv) {
+    public volatile int i = 0;
+    public void increase() {
+        i++;
+    }
 
+    public static void main(String[] argv) {
+        final SynchronizedTest synchronizedTest = new SynchronizedTest();
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(()-> {
+                for (int j = 0; j < 1000; j++) {
+                    synchronizedTest.increase();
+                }
+            });
+            t.start();
+        }
+        while(Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+        System.out.println(synchronizedTest.i);
     }
 }
