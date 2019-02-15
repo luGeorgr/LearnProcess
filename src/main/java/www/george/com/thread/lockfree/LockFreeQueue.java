ba1f5f219e2d585c284da1995d4756b7391a3038
rouqiu <rouqiu@qunhemail.com>
2019-02-15 14:42:48 +0800

    begin lock free queue

--- src/main/java/www/george/com/thread/lockfree/LockFreeQueue.java
+++ src/main/java/www/george/com/thread/lockfree/LockFreeQueue.java
@@ -5,29 +5,15 @@ package www.george.com.thread.lockfree;
  *
  */
 public class LockFreeQueue {
-    private static final sun.misc.Unsafe UNSAFE = sun.misc.Unsafe.getUnsafe();
     private Node head = new Node();
     private Node tail = head;
     public void enqueueV1(int val) {
         final Node q = new Node(val);
+        Node p;
+        do {
+            p = tail;
+        } while ()
     }
 
 }
 
-class Node {
-    public int val;
-    public Node next;
-
-    public Node(final int val, final Node node) {
-        this.val = val;
-        next = node;
-    }
-
-    public Node(final int val) {
-        this(val, null);
-    }
-
-    public Node() {
-        this(0);
-    }
-}
