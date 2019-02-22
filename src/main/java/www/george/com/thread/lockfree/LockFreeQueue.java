017895766443ad6da6b4e8a72d744a99546bb5f9
rouqiu <rouqiu@qunhemail.com>
2019-02-22 09:22:38 +0800

    begin write LockFreeQueue

--- src/main/java/www/george/com/thread/lockfree/LockFreeQueue.java
+++ src/main/java/www/george/com/thread/lockfree/LockFreeQueue.java
@@ -1,19 +1,62 @@
 package www.george.com.thread.lockfree;
 
+import sun.misc.Unsafe;
+
 /**
  * @author rouqiu
  *
  */
-public class LockFreeQueue {
-    private Node head = new Node();
-    private Node tail = head;
-    public void enqueueV1(int val) {
-        final Node q = new Node(val);
-        Node p;
-        do {
-            p = tail;
-        } while ()
+public class LockFreeQueue<T> {
+    private volatile Node<T> head;
+    private volatile Node<T> tail;
+
+    public LockFreeQueue() {
+        this.head = this.tail = new Node<>(null);
     }
 
+//    public void add(final T value) {
+//
+//    }
+//
+//    public T remove() {
+//
+//    }
+//
+//    public int size() {
+//
+//    }
+//
+//    public boolean empty() {
+//
+//    }
+
+    private static class Node<T> {
+        private T value;
+        private volatile Node<T> next;
+        private static final Unsafe UNSAFE;
+        private static final long VALUE_OFFSET;
+        private static final long NEXT_OFFSET;
+
+        static {
+            try {
+                UNSAFE = Unsafe.getUnsafe();
+                Class<?> k = Node.class;
+                VALUE_OFFSET = UNSAFE.objectFieldOffset(k.getDeclaredField("value"));
+                NEXT_OFFSET = UNSAFE.objectFieldOffset(k.getDeclaredField("next"));
+            } catch (Exception e) {
+                throw new Error(e);
+            }
+        }
+
+        public Node(final T value, final Node<T> next) {
+            this(next);
+            this.value = value;
+        }
+
+        public Node(final Node<T> next) {
+            this.next = next;
+        }
+
+    }
 }
 
