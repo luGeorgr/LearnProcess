62c45e4603f6f469dc30b2e4f944a910dfd7b434
陆波 <570562634@qq.com>
2020-06-20 20:25:03 +0800

    learn noi

---
+++ src/main/java/www/george/com/netty/nio/MultiplexerTimeServer.java
@@ -0,0 +1,79 @@
+package www.george.com.netty.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MultiplexerTimeServer implements Runnable {
+    private Selector selector;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    private volatile boolean stop;
+
+    public MultiplexerTimeServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("The time Server is start in port: " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    final SelectionKey key = iterator.next();
+                    //看起来NIO使用的key需要自己手动删除
+                    iterator.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) {
+        if (!key.isValid()) {
+            return;
+        }
+    }
+}
--- src/main/java/www/george/com/netty/nio/TimeServer.java
+++ src/main/java/www/george/com/netty/nio/TimeServer.java
@@ -1,4 +1,8 @@
 package www.george.com.netty.nio;
 
 public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+
+    }
 }
